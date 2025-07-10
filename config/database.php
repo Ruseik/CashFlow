c50b0270b7a5a52d8b279b8efc1155d0cc4171c5
Ruseik <asdwrusek@gmail.com>
2025-07-10 11:08:28 +0530

    Added advanced analytics section for expenditure analysis

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'host' => 'localhost',
-    'dbname' => 'budget_db',
-    'username' => 'budget_user',
-    'password' => 'asdw10010',
+    'dbname' => 'your database name',
+    'username' => 'your database username',
+    'password' => 'your password for the database user',
     'charset' => 'utf8mb4',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
